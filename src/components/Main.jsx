2064a2326b5ccd778faaa669aba4fc810181f2e9
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-12 16:43:21 +0200

    examples of conditional rendering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,15 +29,34 @@ function Main(){
 
     }
 
+    let message;
+    if(moviesToDisplay.length > 0){
+        message = <h1>Number of movies: {moviesToDisplay.length}</h1>
+    } else {
+        message = <h1>Sorry, no movies to display</h1>
+    }
+
+
     return (
         <div className="Main">
-            <h1>Number of movies: {moviesToDisplay.length}</h1>
+            
+            {message}
 
             {moviesToDisplay.map((movieObj) => {
                 return (
                     <section key={movieObj.id} className="card" >
                         <h2>{movieObj.title}</h2>
+
+                        { movieObj.imgURL 
+                            ? <img src={movieObj.imgURL} /> 
+                            : <img src="https://dummyimage.com/182x268/ffffff/000000" /> 
+                        }
+
+                        
+
+
                         <h3>Rating: {movieObj.rating}</h3>
+                        {movieObj.rating > 8 && <p>RECOMMENDED</p>}
 
                         <button onClick={() => {deleteMovie(movieObj.id)}}>Delete</button>
                     </section>
