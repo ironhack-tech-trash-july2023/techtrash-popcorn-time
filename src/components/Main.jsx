f441529d16c813822dbe02de8508e665dd67b039
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-12 16:23:31 +0200

    display list of movies + functionality to delete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import movies from "../data/movies.json";
+
+import "./Main.css";
+
 function Main(){
-    return <h1>this is the Main</h1>;
+
+    // const [variable, setVariable] = useState(initalValue);
+
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+    const deleteMovie = (movieId) => {
+
+        const newList = moviesToDisplay.filter( (element) => {
+            if(element.id !== movieId){
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+
+        // const newList = moviesToDisplay.filter( (element) => {
+        //     return element.id !== movieId;
+        // });
+
+
+        setMoviesToDisplay(newList);
+
+    }
+
+    return (
+        <div className="Main">
+            <h1>Number of movies: {moviesToDisplay.length}</h1>
+
+            {moviesToDisplay.map((movieObj) => {
+                return (
+                    <section key={movieObj.id} className="card" >
+                        <h2>{movieObj.title}</h2>
+                        <h3>Rating: {movieObj.rating}</h3>
+
+                        <button onClick={() => {deleteMovie(movieObj.id)}}>Delete</button>
+                    </section>
+                )
+            })}
+        </div>
+    );
 }
 
 export default Main;
