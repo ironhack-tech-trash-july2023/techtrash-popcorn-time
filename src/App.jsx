cda0bceca0db2eaba4bcac2fdc255262eeee5406
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-13 15:12:37 +0200

    (forms) functionality to add new movies

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,73 @@ function App() {
 
   const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
   
-  const deleteMovie = (movieId) => {
+
+  const deleteMovie = (movieTitle) => {
     const newList = moviesToDisplay.filter( (element) => {
-        return element.id !== movieId;
+        return element.title !== movieTitle;
     });
     setMoviesToDisplay(newList);
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const newMovie = {
+      title: title,
+      rating: rating
+    }
+
+    const newList = [newMovie, ...moviesToDisplay];
+    
+    setMoviesToDisplay(newList);
+
+    // clear form
+    setTitle("");
+    setRating("");
   }
 
 
   return (
     <>
       <Header numberOfMovies={moviesToDisplay.length} />
+
+      <section>
+        <form onSubmit={handleSubmit}>
+          
+          <label>
+            Title: 
+            <input 
+              type="text" 
+              name="title" 
+              required={true}
+              placeholder="enter the title" 
+              value={title} 
+              onChange={(e) => { setTitle(e.target.value) }}
+            />
+          </label>
+          
+          
+          <label>
+            Rating:
+            <input 
+              name="rating"
+              type="number"
+              required={true}
+              min={1}
+              max={10}
+              value={rating}
+              onChange={(e) => { setRating(e.target.value) }}
+            />
+          </label>
+
+          <button>Create</button>
+        </form>
+      </section>
+
       <Main movies={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </>
